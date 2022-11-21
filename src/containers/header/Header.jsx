b147ea8f46e7e6bd4083c63cf49d0805f49ba354
nitin <nitinvardha09@gmail.com>
2022-11-21 17:06:09 -0600

    header paragraph completed 50%

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './header.css'
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
@@ -10,6 +12,22 @@ const Header = () => {
         amazing with GPT-3
         OpenAI
         </h1>
+        <p>
+        Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+        </p>
+        <div className='gpt3_header-content__input'>
+          <input type='email' placeholder='Your Email Address'  />
+          <button type='button' >Get Strated</button>
+        </div>
+
+        <div className="gpt3_header-content__people">
+          <img src={people} />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className='gpt3__header-image'>
+          <img src={ai} alt="ai" />
+        </div>
       </div>
     </div> 
   )
